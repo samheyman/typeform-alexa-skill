fd47901de2ec27cdd872468525f16ece65f679cd
Sam Heyman <sam.heyman@gmail.com>
2018-12-11 15:04:53 +0100

    Working app with Typeform APIs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 import os
 from flask import Flask
 from flask_ask import Ask, request, session, question, statement
+from typeform import get_questions, get_responses, get_average_rating, count_true_false
 
 
 app = Flask(__name__)
@@ -11,29 +12,33 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @ask.launch
 def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = 'Welcome to the Alexa Typeform Skill. You can ask me what your user feedback is, or you can ask me to remind you what the questions are.'
+    return question(speech_text).reprompt(speech_text).simple_card('app', speech_text)
 
-@ask.intent('Questions')
+@ask.intent('GetQuestions')
 def return_questions():
-    questions = ['How would you rate your stay?', 'Would you come again?', 'Would you recommend us?']
-    speech_text = 'The questions you asked are'
+    questions = get_questions()
+    speech_text = 'The questions you asked are. '
     for question in questions:
-        speech_text = speech_text + question
+        speech_text = speech_text + ' ' + str(question['text'])
     return statement(speech_text).simple_card('Questions', speech_text)
 
 @ask.intent('CustomerFeedback')
 def return_feedback():
-    questions = [" How would you rate your stay? ", ". Would you come again? ", " Would you recommend us? "]
-    responses = []
-    i = 1
-    responses.append(". 5 ")
-    responses.append(" Your average rating is 3 point 5. ")
-    responses.append(" 2 out of 5 said yes. ")
-    responses.append(" 3 out of 5 said yes. ")
-    speech_text = "You have had {} responses in total. Here are the answers: ".format(responses[0])
+    questions = get_questions()
+    responses = get_responses()
+    i=0
+    speech_text = "You have had {} responses in total. Here are the answers: ".format(len(responses))
     for question in questions:
-        speech_text = " " + speech_text + " " + question + " " + responses[i] + " " 
+        print("Question: {}".format(question['text']))
+        if question['type'] == 'rating':
+            average_rating = get_average_rating(responses, question['id'])
+            answer = "Your average rating is {} .".format(average_rating)
+        else:
+            count = count_true_false(responses, question['id'])
+            answer = "{} out of {} said yes.".format(count[0], count[0] + count[1])
+        print("The answer is: {} (type: {})".format(answer, type(answer)))
+        speech_text = speech_text + " " + question['text'] + " " + answer + " " 
         i+=1
     return statement(speech_text).simple_card('Responses', speech_text)
 
@@ -50,4 +55,5 @@ def session_ended():
 
 
 if __name__ == '__main__':
+    return_questions()
     app.run(debug=True)
